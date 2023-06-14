9215785c1865fe951ad1749f3ba4b97230d62906
mateu <mateu@LAPTOP-KD9LPAJO>
2023-06-14 13:22:37 -0300

    testando

---
+++ Gustavo/programa1/src/ClassePrincipal.java
@@ -0,0 +1,12 @@
+
+public class ClassePrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			System.out.println("olá, mundo!");
+			System.out.println("estou começando a aprender");
+	}
+	
+
+}
---
+++ Gustavo/src/main/java/mm/gustavo/Gustavo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mm.gustavo;
+
+/**
+ *
+ * @author mateu
+ */
+public class Gustavo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
